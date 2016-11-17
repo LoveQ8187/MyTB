403f0ae2ee5666582500df4824ff8b8e8d427f66
chenghao <763561902@qq.com>
2016-11-17 18:52:46 +0800

    点击返回键返回上一fragment

--- app/src/main/java/com/example/chenghao/mytb/Utils/CookListAdapter.java
+++ app/src/main/java/com/example/chenghao/mytb/Utils/CookListAdapter.java
@@ -1,17 +1,36 @@
 package com.example.chenghao.mytb.Utils;
 
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+
+import java.util.List;
+import java.util.Map;
+import java.util.zip.Inflater;
 
 /**
  * Created by chenghao on 2016/11/15.
  * 自定义adapter，用于异步加载做菜步骤中的图片
  */
 public class CookListAdapter extends BaseAdapter {
+
+    private List<Map>itemList;//存储列表需要显示的数据
+    private int layoutId;//list单行的布局id
+    private Context mContext;
+    private LayoutInflater mInflater;//获取LayoutInfalter对象导入布局
+
+    public CookListAdapter(Context mContext,List<Map>itemList, int layoutId){
+        this.mContext=mContext;
+        this.itemList=itemList;
+        this.layoutId=layoutId;
+        mInflater= LayoutInflater.from(this.mContext);
+    }
     @Override
     public int getCount() {
-        return 0;
+        return itemList.size();
     }
 
     @Override

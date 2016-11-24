d592e8561d95316395cc1a95e7c5f00118d18ebf
chenghao <763561902@qq.com>
2016-11-24 16:16:05 +0800

    完成cook查询功能

--- app/src/main/java/com/example/chenghao/mytb/Fragments/BackHandleFragment.java
+++ app/src/main/java/com/example/chenghao/mytb/Fragments/BackHandleFragment.java
@@ -32,7 +32,7 @@ public abstract class BackHandleFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_back_handle, container, false);
+        return null;
     }
 
 }
